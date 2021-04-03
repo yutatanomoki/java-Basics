dbee13b814334c316bbe9303dbd4cdb029aac6b1
yutatanomoki <yuta3071@gmail.com>
2021-04-03 21:22:54 +0900

    [Start]Java基礎

---
+++ basis.java
@@ -0,0 +1,14 @@
+//入力した回数分
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "paiza";
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        for(int i = 0;i<n; i++){
+            System.out.println(name);
+        }
+    }
+}
