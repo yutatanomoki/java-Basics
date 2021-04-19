03410ee3a229f961121cc8e0286ae551b27dbd02
yutatanomoki <yuta3071@gmail.com>
2021-04-20 07:49:54 +0900

    [add]java

--- basis.java
+++ basis.java
@@ -101,3 +101,19 @@ public class Main {
         System.out.println(sum);
     }
 }
+//入力した数値、名前の占い結果表示
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("ようこそ占いの館へ");
+		System.out.println("あなたのの名前を入力してください");
+		String name = new java.util.Scanner(System.in).nextLine();
+		System.out.println("あなたの年齢を入力してください");
+		int ageString = new java.util.Scanner(System.in).nextInt();
+		int age = ageString;
+		int fortune = new java.util.Random().nextInt(4);
+		fortune = fortune + 1;
+		System.out.println("占いの結果が出ました！");
+		System.out.println(age+"歳の"+name+"さんあなたの運気番号は"+fortune+"です");
+		System.out.println("1:大吉 2中吉 3:吉 4:凶 ");
+	}
+}
