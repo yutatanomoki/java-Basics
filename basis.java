02650d6d0e8ebfb0bf099b08d0e197dd4f09abab
yutatanomoki <yuta3071@gmail.com>
2021-04-25 23:53:51 +0900

    [add]円、三角形の面積算出

--- basis.java
+++ basis.java
@@ -151,8 +151,7 @@ public class Main {
 public class Main {
 	public static void main(String[] args) {
             double triangleArea = calcTriangleArea(10.0,5.0);
-		    System.out.println("三角形の面積"+triangleArea+"平方cm");
-		    
+		    System.out.println("三角形の面積"+triangleArea+"平方cm");    
 		    double circleArea = calcCircleArea(5.0);
 		    System.out.println("円の面積"+circleArea+"平方cm");
 	}
